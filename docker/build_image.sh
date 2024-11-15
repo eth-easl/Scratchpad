f75a9ab38f800e4b1c8775e1732bab28f0704bc1
Xiaozhe Yao <askxzyao@gmail.com>
2024-11-15 14:21:52 +0100

    hotfix #11

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,11 +1,12 @@
 # get cpu arch
 arch=$(uname -m)
 version=$1
+buildtool=$2
 # if version is not provided, raise error
 if [ -z "$version" ]; then
     echo "Please provide version number"
     exit 1
 fi
 echo "Building image for $arch, version $version"
-podman build -f docker/Dockerfile.$arch-cuda . -t ghcr.io/xiaozheyao/scratchpad:${version}dev-$arch --build-arg ARCH=$arch
-podman push ghcr.io/xiaozheyao/scratchpad:${version}dev-$arch
+$buildtool build -f docker/Dockerfile.$arch-cuda . -t ghcr.io/xiaozheyao/scratchpad:${version}dev-$arch --build-arg ARCH=$arch
+$buildtool push ghcr.io/xiaozheyao/scratchpad:${version}dev-$arch
