95833f3e4d80ce63cea5434422b47d0b5fc38123
Xiaozhe Yao <askxzyao@gmail.com>
2024-12-01 18:31:31 +0100

    minor

---
+++ scripts/serve_llama_1b_with_toppings_torch.sh
@@ -0,0 +1,4 @@
+export PROMETHEUS_MULTIPROC_DIR=.local
+sp serve meta-llama/Llama-3.2-1B-Instruct --host 0.0.0.0 --port 8080 \
+--enable-system-controller --use-heterogeneous-pool \
+--init-toppings lora:ketchup123/llama-3.2-1B-instruct-gsm8k:ketchup123/llama-3.2-1B-instruct-gsm8k --attention-backend triton --sampling-backend torch
