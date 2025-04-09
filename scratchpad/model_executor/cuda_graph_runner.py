fix gemma3 for causal LM

scratchpad/model_executor/cuda_graph_runner.py
@@ -69,7 +69,7 @@ def patch_model(
             yield torch.compile(
                 torch.no_grad()(model.forward),
                 mode=os.environ.get(
-                    "SGLANG_TORCH_COMPILE_MODE", "max-autotune-no-cudagraphs"
+                    "SP_TORCH_COMPILE_MODE", "max-autotune-no-cudagraphs"
                 ),
                 dynamic=False,
             )
@@ -256,7 +256,6 @@ class CudaGraphRunner:
                 "2. set --mem-fraction-static to a smaller value (e.g., 0.8 or 0.7)\n"
                 "3. disable torch compile by not using --enable-torch-compile\n"
                 "4. set --cuda-graph-max-bs to a smaller value (e.g., 32)\n"
-                "Open an issue on GitHub https://github.com/sgl-project/sglang/issues/new/choose \n"
             )
 
     @contextmanager

scratchpad/model_executor/model_runner.py
@@ -190,7 +190,7 @@ class ModelRunner:
                 )
                 self.server_args.dtype = "float16"
                 if torch.cuda.get_device_capability()[1] < 5:
-                    raise RuntimeError("SGLang only supports sm75 and above.")
+                    raise RuntimeError("SP only supports sm75 and above.")
 
         # Prepare the vllm model config
         self.load_config = LoadConfig(load_format=self.server_args.load_format)
@@ -551,9 +551,10 @@ class ModelRunner:
         # Sample the next tokens
         next_token_ids = self.sampler(
             logits_output,
-            sampling_info,
+            forward_batch.sampling_info,
             forward_batch.return_logprob,
             forward_batch.top_logprobs_nums,
+            forward_batch.token_ids_logprobs,
         )
         return next_token_ids
 